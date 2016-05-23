c66109047df478bab45cd0a07149c14a1f5b2b87
Butschster <butschster@gmail.com>
2016-05-23 16:06:27 +0300

    fix permissions

--- src/resources/lang/ru/permissions.php
+++ src/resources/lang/ru/permissions.php
@@ -6,7 +6,7 @@ return [
         'api' => 'Api',
     ],
     'api' => [
-        'view_key' => 'Просмотр списка',
+        'view_keys' => 'Просмотр списка',
         'refresh_key' => 'Обновление ключа',
         'create_keys' => 'Создание',
         'delete_keys' => 'Удаление',
